aa7fb58e06e5295ed26e2f701ee9d8eff6461948
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-01-06 11:10:08 +0800

    Update Responsive_Form (Add Commen)

--- Responsive_Form/app.js
+++ Responsive_Form/app.js
@@ -2,10 +2,12 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
+// Add the "sign-up-mode" into the classlist of the container when user click "Sign Up"
 sign_up_btn.addEventListener('click', () =>{
     container.classList.add("sign-up-mode");
 })
 
+// Remove the "sign-up-mode" from the classlist of the container when user click "Sign In"
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 })

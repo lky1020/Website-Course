a308919f6ecb6ebd6ba918be4426fed64038f828
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-01-16 13:22:10 +0800

    Complete Razor Website

---
+++ Asp.Net/BookListRazor/BookListRazor/Migrations/20210116030055_AddISBNToBookModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Book",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "author",
+                table: "Book",
+                newName: "Author");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Book",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Book",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Author",
+                table: "Book",
+                newName: "author");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Book",
+                newName: "id");
+        }
+    }
+}
